f95857fbf69d8404868f30b7d6a4254f6b4c462c
Tashakashkina <tashakashkina@gmail.com>
2022-04-26 02:03:17 +0300

    добавлен сплеш-эффект для кнопки назад

--- lib/widgets/details.dart
+++ lib/widgets/details.dart
@@ -8,7 +8,15 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(240, 240, 238, 1),
       appBar: AppBar(
-          leading: BackButton(color: Color.fromRGBO(35, 33, 34, 1)),
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Color.fromRGBO(35, 33, 34, 1),
+              splashRadius: 50,
+              splashColor: Colors.grey,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/thirdPage');
+              },
+              tooltip: "Назад"),
           backgroundColor: Color.fromRGBO(174, 213, 243, 1),
           title: Text('Мероприятия',
               style: TextStyle(color: Color.fromRGBO(35, 33, 34, 1)))),
