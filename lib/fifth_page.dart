ae3bf60a89782a3b5479caad627ca144a4dce446
letsmespeakfrommyheart <maria@MacBook-Air-Vladimir.local>
2022-04-26 07:58:56 +0300

    added fifth page as comments

--- lib/fifth_page.dart
+++ lib/fifth_page.dart
@@ -1,5 +1,143 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "Map Page",
+//         theme: ThemeData(
+//           primarySwatch: Colors.lightGreen,
+//         ),
+//         home: const MapPage());
+//   }
+// }
+
+// class MapPage extends StatefulWidget {
+//   const MapPage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MapPage> createState() => _MapPageState();
+// }
+
+// class _MapPageState extends State<MapPage> {
+//   Set<Marker> markers = {};
+//   Completer<GoogleMapController> _controller = Completer();
+//   double zoomValue = 17.0;
+//   final _mainTarget = LatLng(54.10128, 37.57868); //add main target of screen
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _addMarkerMainTarget();
+//   }
+
+//   Widget _zoomMinusButton() {
+//     // widget - button - minus
+//     return FloatingActionButton(
+//         materialTapTargetSize: MaterialTapTargetSize.padded,
+//         backgroundColor: Colors.lightGreen,
+//         child: const Text("-"),
+//         onPressed: () {
+//           zoomValue--;
+//           _minus(zoomValue);
+//         });
+//   }
+
+//   Widget _zoomPlusButton() {
+//     //widget - button - plus
+//     return FloatingActionButton(
+//         materialTapTargetSize: MaterialTapTargetSize.padded,
+//         backgroundColor: Colors.lightGreen,
+//         child: const Text("+"),
+//         onPressed: () {
+//           zoomValue++;
+//           _plus(zoomValue);
+//         });
+//   }
+
+//   Future<void> _minus(double zoomVal) async {
+//     //function of animate camera '-'
+//     final GoogleMapController controller = await _controller.future;
+//     controller.animateCamera(CameraUpdate.newCameraPosition(
+//         CameraPosition(target: _mainTarget, zoom: zoomVal)));
+//   }
+
+//   Future<void> _plus(double zoomVal) async {
+//     //function of animate camera '+'
+//     final GoogleMapController controller = await _controller.future;
+//     controller.animateCamera(CameraUpdate.newCameraPosition(
+//         CameraPosition(target: _mainTarget, zoom: zoomVal)));
+//   }
+
+//   void _addMarkerMainTarget() {
+//     markers.add(Marker(
+//       markerId: MarkerId('main target'),
+//       position: _mainTarget,
+//       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+//     ));
+
+//     setState(() {});
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Местоположение",
+//             style: TextStyle(fontSize: 20, fontFamily: 'Lato')),
+//         centerTitle: false,
+//       ),
+//       body: Stack(
+//         alignment: AlignmentDirectional.bottomCenter,
+//         children: [
+//           GoogleMap(
+//             initialCameraPosition: CameraPosition(
+//               target: _mainTarget,
+//               zoom: 17,
+//             ),
+//             onMapCreated: (GoogleMapController controller) {
+//               _controller.complete(controller);
+//             },
+//             zoomControlsEnabled: false,
+//             myLocationButtonEnabled: false,
+//             zoomGesturesEnabled: false,
+//             markers: markers,
+//           ),
+//           Container(
+//             height: 140,
+//             width: double.infinity,
+//             color: const Color.fromRGBO(255, 255, 255, 0.9),
+//             child: const Text(
+//               'Некие подробности',
+//               style: TextStyle(fontSize: 25, fontFamily: 'Lato'),
+//             ),
+//           ),
+//           Padding(
+//             padding: EdgeInsets.all(16.0),
+//             child: Align(
+//               alignment: Alignment.topRight,
+//               child: Column(
+//                 children: <Widget>[
+//                   _zoomPlusButton(),
+//                   SizedBox(
+//                     height: 20,
+//                   ),
+//                   _zoomMinusButton()
+//                 ],
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,38 @@
 // second test lily
 // third test veronika
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: FirstPage(),
+//     );
+//   }
+// }
 
-class FirstPage extends StatefulWidget {
-  @override
-  _FirstPageState createState() => _FirstPageState();
-}
+// class FirstPage extends StatefulWidget {
+//   @override
+//   _FirstPageState createState() => _FirstPageState();
+// }
 
-class _FirstPageState extends State {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 35),
-          child: Column(children: [
-            Image.asset('assets/images/logo.jpg'),
-            const SizedBox()
-          ]),
-        ),
-      ]),
-    );
-  }
-}
+// class _FirstPageState extends State {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(children: <Widget>[
+//         Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 35),
+//           child: Column(children: [
+//             Image.asset('assets/images/logo.jpg'),
+//             const SizedBox()
+//           ]),
+//         ),
+//       ]),
+//     );
+//   }
+// }
