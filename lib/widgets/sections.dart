41bb5dac3012977965543687db796ad29205618e
Tashakashkina <tashakashkina@gmail.com>
2022-04-28 02:13:18 +0300

    Обновления по дизайну стр 3

---
+++ lib/widgets/sections.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Event {
+  final String title;
+  final String description;
+  final String time;
+
+  Event({required this.title, required this.description, required this.time});
+}
