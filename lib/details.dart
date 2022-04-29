1250db7706465273071b7b5db59c67d7fcde4db6
Tashakashkina <tashakashkina@gmail.com>
2022-04-29 17:23:19 +0300

    добавлен фон для страницы 3 и строка поиска

--- lib/details.dart
+++ lib/details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tos_parkovii/fifth_page.dart';
 
 class DetailPage extends StatelessWidget {
   late final int index;
@@ -7,19 +6,19 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(240, 240, 238, 1),
+      backgroundColor: const Color.fromRGBO(240, 240, 238, 1),
       appBar: AppBar(
           leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              color: Color.fromRGBO(35, 33, 34, 1),
+              icon: const Icon(Icons.arrow_back),
+              color: const Color.fromRGBO(35, 33, 34, 1),
               splashRadius: 50,
               splashColor: Colors.grey,
               onPressed: () {
                 Navigator.of(context).pushNamed('/thirdPage');
               },
               tooltip: "Назад"),
-          backgroundColor: Color.fromRGBO(174, 213, 243, 1),
-          title: Text('Мероприятия',
+          backgroundColor: const Color.fromRGBO(174, 213, 243, 1),
+          title: const Text('Мероприятия',
               style: TextStyle(color: Color.fromRGBO(35, 33, 34, 1)))),
       body: Center(
           child: TextButton(
