c0683834ddf56d26ceaf927716ac64a6beee0847
Veronika L <verokipri@yahoo.com>
2022-05-14 22:27:38 +0200

    added helper file

---
+++ lib/helper.dart
@@ -0,0 +1,30 @@
+// мероприятия
+class Event {
+  final String title;
+  final String place;
+  final String time;
+  final String date;
+  final String description;
+  final String type;
+  final dynamic longitude;
+  final dynamic  latitude;
+  final String picture;
+
+  Event({required this.title,
+        required this.place,
+        required this.description,
+        required this.time,
+        required this.date,
+        required this.type,
+        required this.longitude,
+        required this.latitude,
+        required this.picture});
+}
+
+// организации
+
+
+// дома
+
+
+// пространства
